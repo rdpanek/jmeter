4753b496d66c8f01fe5e6d2919df6b1eeadd3a2d
Radim Daniel Panek <rdpanek@gmail.cz>
2020-11-25 09:44:18 +0100

    build-docker-images.sh: added push

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -4,3 +4,6 @@ JMETER_VERSION=5.3.0
 
 docker build --tag="rdpanek/jmeter:${JMETER_VERSION}" .
 docker build --tag="rdpanek/jmeter:vnc-${JMETER_VERSION}" -f ./vnc/Dockerfile .
+
+docker push rdpanek/jmeter:${JMETER_VERSION}
+docker push rdpanek/jmeter:vnc-${JMETER_VERSION}
